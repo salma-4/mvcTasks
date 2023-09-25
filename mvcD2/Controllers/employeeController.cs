0665d368cab69738654d4c62fc4a388327e5263e
Your Name <you@example.com>
2023-09-25 20:07:53 +0200

    day3(countinue crud for employeeand use layout)

--- mvcD2/Controllers/employeeController.cs
+++ mvcD2/Controllers/employeeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using mvcD2.Models;
 
 namespace mvcD2.Controllers
@@ -19,7 +20,7 @@ namespace mvcD2.Controllers
         }
         public IActionResult Details(int id)
         {
-            Employee employee =db.Employees.Where(n=>n.Id== id).FirstOrDefault();
+            Employee employee = db.Employees.Include(c=>c.Office).SingleOrDefault(c=>c.Id==id);
             if(employee == null)
                 return Content("Error");
             else
@@ -27,7 +28,10 @@ namespace mvcD2.Controllers
         }
           public IActionResult addNew()
          {
-            return View(addNew);
+
+           List<Office> office = db.Offices.ToList();
+            ViewBag.Off = office;
+            return View();
         }
         public IActionResult AddToDB(Employee employee) {
             db.Employees.Add(employee);
@@ -35,5 +39,30 @@ namespace mvcD2.Controllers
             return RedirectToAction("Index");
 
         }
+        public IActionResult Edit(int id)
+        {
+            Employee emp = db.Employees.SingleOrDefault(n => n.Id== id);
+            ViewBag.Employee = db.Employees.ToList();
+            return View("Edit",emp);
+        }
+        public IActionResult EditInDB(Employee employee)
+        {
+            Employee oldEmp = db.Employees.SingleOrDefault(n=>n.Id== employee.Id);
+            oldEmp.Name= employee.Name;
+            oldEmp.Address= employee.Address;
+            oldEmp.Age= employee.Age;
+            oldEmp.email = employee.email;
+            oldEmp.Salary= employee.Salary;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Delete(int id) {
+            Employee employee = db.Employees.SingleOrDefault(n=>n.Id==id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
