247c62eca7586b137a0250bb649abbd9e5134f28
Your Name <you@example.com>
2023-09-27 10:00:40 +0200

    day4(offices crud)#

---
+++ mvcD2/Controllers/officeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcD2.Models;
+
+namespace mvcD2.Controllers
+{
+    public class officeController : Controller
+    {
+        private CompanyContext db;
+        public officeController()
+        {
+            db = new CompanyContext();
+        }
+        public IActionResult Index()
+        {
+            List<Office> office = db.Offices.ToList();
+
+            return View(office);
+        }
+        public IActionResult Details(int id)
+        {
+            Office office  = db.Offices.Where(n=>n.Id== id).FirstOrDefault();   
+            if (office == null)
+                return Content("Error");
+            else
+                return View(office);
+        }
+        public IActionResult addNew()
+        {
+
+            List<Office> office = db.Offices.ToList();
+            ViewBag.Off = office;
+            return View();
+        }
+        public IActionResult AddToDB(Office off)
+        {
+            db.Offices.Add(off);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Edit(int id)
+        {
+           Office off= db.Offices.SingleOrDefault(n => n.Id == id);
+            ViewBag.Office = db.Offices.ToList();
+            return View("Edit",off);
+        }
+        public IActionResult EditInDB(Office office)
+        {
+            Office oldOFF = db.Offices.SingleOrDefault(n => n.Id == office.Id);
+            oldOFF.Name = office.Name;
+            oldOFF.Location = office.Location;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+        public IActionResult Delete(int id)
+        {
+           
+            return RedirectToAction("Index");
+        }
+    }
+}
