52892f26ec4eff9884ea0ce7ad3686d4c85c0a01
Your Name <you@example.com>
2023-10-01 19:35:51 +0200

    day5(using view models ,validation of employee)

---
+++ mvcD2/Controllers/employeeValidationController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvcD2.Models;
+using mvcD2.ViewModels;
+
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace mvcD2.Controllers
+{
+    public class employeeValidationController : Controller
+    {
+        private CompanyContext db;
+        public employeeValidationController()
+        {
+            db = new CompanyContext();
+        }
+        // add employee with validation
+        [HttpGet]
+        public IActionResult Add()
+        {
+            employeeVM emp = new employeeVM()
+            {
+                offices = new SelectList(db.Offices,"Id","Name")
+            };
+            return View(emp);
+        }
+
+        [HttpPost]
+        public IActionResult Add( employeeVM employee)
+        {
+            if(ModelState.IsValid)
+            {
+                Employee emp= new Employee()
+                {
+                    Name = employee.Name,
+                    Age = employee.Age,
+                    Address = employee.Address,
+                    password =employee.password,
+                    Salary = employee.Salary,
+                    email = employee.email,
+                    office_id = employee.office_id,
+                };
+                db.Employees.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index", "employee");
+            }
+            employee.offices = new SelectList(db.Offices, "Id", "Name");
+                return View(employee);
+           
+        }
+    }
+}
---
+++ mvcD2/ViewModels/employeeVM.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using mvcD2.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvcD2.ViewModels
+{
+    public class employeeVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(23,55)]
+        public int Age { get; set; }
+        public string? Address { get; set; }
+        public double? Salary { get; set; }
+        [Required]
+        public string? email { get; set; }
+
+        [RegularExpression("^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z0-9 ]{8,}$")]
+        //Minimum eight characters, at least one letter and one number
+        public string password { get; set; }
+        [Compare("password")]
+        public string confirmPassword { get; set; }
+
+        [Display(Name="Office")]
+        public int? office_id { get; set; }
+        public SelectList offices { get; set; }
+
+
+    }
+}
